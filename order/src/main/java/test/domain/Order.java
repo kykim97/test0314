26c66c6c5c7b24649139e95de2f5c9239ee3167c
kykim97 <113568664+kykim97@users.noreply.github.com>
2023-03-14 10:47:56 +0900

    commit

---
+++ order/src/main/java/test/domain/Order.java
@@ -0,0 +1,35 @@
+package test.domain;
+
+import java.util.Date;
+import java.util.List;
+import javax.persistence.*;
+import lombok.Data;
+import test.OrderApplication;
+import test.domain.OrderPlaced;
+
+@Entity
+@Table(name = "Order_table")
+@Data
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String productName;
+
+    private Integer qty;
+
+    @PostPersist
+    public void onPostPersist() {
+        OrderPlaced orderPlaced = new OrderPlaced(this);
+        orderPlaced.publishAfterCommit();
+    }
+
+    public static OrderRepository repository() {
+        OrderRepository orderRepository = OrderApplication.applicationContext.getBean(
+            OrderRepository.class
+        );
+        return orderRepository;
+    }
+}
---
+++ order/src/main/java/test/domain/OrderPlaced.java
@@ -0,0 +1,23 @@
+package test.domain;
+
+import java.util.*;
+import lombok.*;
+import test.domain.*;
+import test.infra.AbstractEvent;
+
+@Data
+@ToString
+public class OrderPlaced extends AbstractEvent {
+
+    private Long id;
+    private String productName;
+    private Integer qty;
+
+    public OrderPlaced(Order aggregate) {
+        super(aggregate);
+    }
+
+    public OrderPlaced() {
+        super();
+    }
+}
